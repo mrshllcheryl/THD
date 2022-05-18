2f52066b5378b5aaea58af701c461cf1f6493dbf
mrshllcheryl <62977213+mrshllcheryl@users.noreply.github.com>
2022-05-18 19:39:05 +0800

    Update Profile-Reload.js

--- panel/JS/Profile-Reload.js
+++ panel/JS/Profile-Reload.js
@@ -7,7 +7,7 @@ GitHub:https://github.com/mieqq/mieqq
 * 配置重载 = title=配置重载,content=配置重载,style=info,script-name=配置重载,update-interval=-1
 
 * [Script]
-* 配置重载=script-path=https://raw.githubusercontent.com/TributePaulWalker/Profiles/main/JavaScript/Surge/Profile-Reload.js,type=generic
+* 配置重载=script-path=https://raw.githubusercontent.com/mrshllcheryl/THD/main/panel/JS/Profile-Reload.js,type=generic
 */
 
 $httpAPI("POST", "/v1/profiles/reload", {}, data => {
